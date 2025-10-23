4104258d35a9528745973d9a19efd3f4446518c1
Bpost129 <Bpost129@yahoo.com>
2025-10-22 22:29:25 -0400

    test push, pop, shift, and unshift

--- app.js
+++ app.js
@@ -7,3 +7,8 @@ let color2 = colors[1]
 console.log(color2)
 colors.push('yellow', 'purple')
 console.log(colors)
+colors.unshift('pink')
+console.log(colors)
+colors.pop()
+colors.shift()
+console.log(colors)
