55bdbff6f94de2fb84f667bd5df604392752358f
Bpost129 <Bpost129@yahoo.com>
2025-10-23 13:07:26 -0400

    test for loop on arrays

--- app.js
+++ app.js
@@ -13,9 +13,18 @@
 // colors.shift()
 // console.log(colors)
 
-let movies = ['The Matrix', 'Inception', 'Interstellar', 'The Dark Knight', 'Caddyshack', 'Iron Man']
+// let movies = ['The Matrix', 'Inception', 'Interstellar', 'The Dark Knight', 'Caddyshack', 'Iron Man']
 
-let removedMovies = movies.splice(0,3)
-removedMovies = movies.splice(1, 0, 'Forrest Gump')
-console.log(removedMovies)
-console.log(movies)
+// let removedMovies = movies.splice(0,3)
+// removedMovies = movies.splice(1, 0, 'Forrest Gump')
+// console.log(removedMovies)
+// console.log(movies)
+
+let nums = [2, 3, 2]
+let sum = 0
+
+for (let i = 0; i < nums.length; i++) {
+  sum += nums[i]
+}
+
+console.log(sum)
