39620cc72954e1563f96c22e2edc13d5f24c7a12
Bpost129 <Bpost129@yahoo.com>
2025-10-22 23:38:23 -0400

    test splice method

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-let colors = ['red', 'green', 'blue']
+// let colors = ['red', 'green', 'blue']
 
-let nums = new Array(2, 4, 10)
+// let nums = new Array(2, 4, 10)
 
-let color2 = colors[1]
+// let color2 = colors[1]
 
-console.log(color2)
-colors.push('yellow', 'purple')
-console.log(colors)
-colors.unshift('pink')
-console.log(colors)
-colors.pop()
-colors.shift()
-console.log(colors)
+// console.log(color2)
+// colors.push('yellow', 'purple')
+// console.log(colors)
+// colors.unshift('pink')
+// console.log(colors)
+// colors.pop()
+// colors.shift()
+// console.log(colors)
+
+let movies = ['The Matrix', 'Inception', 'Interstellar', 'The Dark Knight', 'Caddyshack', 'Iron Man']
+
+let removedMovies = movies.splice(0,3)
+removedMovies = movies.splice(1, 0, 'Forrest Gump')
+console.log(removedMovies)
+console.log(movies)
